9dfe428f734700e92732b4bad4ab31f4c95045d9
Aryan shinde <shindearyan179@gmail.com>
2025-01-09 21:16:26 +0530

    Optimized Landing Page Folder Structure

--- app/(landing-pages)/page.js
+++ app/(landing-pages)/page.js
@@ -3,7 +3,7 @@ import TopNavbar from "@/components/TopNavbar";
 export default function Home() {
   return (
     <div>
-      <TopNavbar />
+      {/* <TopNavbar /> */}
       <div className="flex h-[300px] flex-col items-center justify-center gap-6 text-xl font-semibold p-6 md:p-10">
         Landing Page
       </div>
